d3e98353e20155d68506e65929355eaef00bbbb8
Zak Fonseca <frederickraul@hotmail.com>
2024-04-12 18:00:52 -0700

    INIT

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,16 @@ import Form from './components/Form';
 import useRentModal from '@/app/hooks/useRentModal';
 
 
-const page = () => {
+const Home = () => {
   const rentModal = useRentModal();
 
-  const Ref = useRef<null | HTMLDivElement>(null);
+  const ref = useRef<null | HTMLDivElement>(null);
   const [VIN, setVIN] = useState('');
   const [isVinValid, setisVinValid] = useState(false);
   
 
   const goForm = () => {
-    Ref.current?.scrollIntoView({ behavior: 'smooth' });
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   const handleVinManage=()=>{
@@ -66,7 +66,47 @@ const page = () => {
   return (
     <ClientOnly>
       <Container>
- <div></div>
+        <div ref={ref} className="
+            w-[700px] 
+            lg:w-[800px] 
+            mx-auto 
+            min-h-[200px
+            pt-12
+            pb-20
+            ">
+          <div className='my-10'>
+            <div className='text-5xl font-bold mt-10 sm:mt-20 md:mt-10'>Get A Quote</div>
+          </div>
+
+          <div>
+            <div className="font-bold text-lg">To receive an offer for your car, please fill out the form below. Make sure to include the correct
+              <span className="text-red-500"> Vehicle ID Number (VIN)</span>.
+            </div>
+            <div className='pt-2'>
+              <InputUnregistered
+                label="Please enter your VIN"
+                //disabled={isLoading}
+                onChange={(e) => {setValue(e.target.value) }}
+                required
+              />
+              {isVinValid ?
+              <span className='text-sm text-green-500'>Valid VIN</span>
+              :
+              <span className='text-sm text-red-500'>Invalid VIN</span>
+               }
+            </div>
+            <div className='py-4'>
+              <Button
+                full
+                // disabled={isLoading}
+                label='Next'
+                onClick={handleVinManage}
+              //onClick={handleSubmit}
+              />
+            </div>
+          </div>
+
+        </div>
       </Container>
 
       <Container full>
@@ -197,4 +237,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Home
