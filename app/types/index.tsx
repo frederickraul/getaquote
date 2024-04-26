04eda19c14bf8f898876392cea560e704ef7ef91
Zak Fonseca <frederickraul@hotmail.com>
2024-04-26 16:08:56 -0700

    - Add Dashboard

--- app/types/index.tsx
+++ app/types/index.tsx
@@ -1,26 +1,5 @@
-import { Listing, Product, Reservation, User } from "@prisma/client";
+import {  User } from "@prisma/client";
 
-export type SafeListing = Omit<Listing, "createdAt"> & {
-  createdAt: string;
-};
-
-export type SafeProduct = Omit<Product, "createdAt"> & {
-  createdAt: string;
-};
-
-export type SafeReview = Omit<Product, "createdAt"> & {
-  createdAt: string;
-};
-
-export type SafeReservation = Omit<
-  Reservation, 
-  "createdAt" | "startDate" | "endDate" | "listing"
-> & {
-  createdAt: string;
-  startDate: string;
-  endDate: string;
-  listing: SafeListing;
-};
 
 export type SafeUser = Omit<
   User,
